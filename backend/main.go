872463da7b627a74e2f9084e6d1b7ce11c13cae3
rotpingo <viorel.moschoi@gmail.com>
2025-07-30 14:15:11 +0200

    first commit

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, Gin!",
+		})
+	})
+
+	// Start server on port 6969
+	r.Run(":6969")
+}
