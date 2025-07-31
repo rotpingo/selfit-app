ebce85691a7faa0dbc3e61927e0b7cf1ce78e082
rotpingo <viorel.moschoi@gmail.com>
2025-07-31 11:19:39 +0200

    moved some logic to controllers package, created user model, changed docker-compose.yml

--- backend/main.go
+++ backend/main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"gin/config"
 	"gin/database"
-	"gin/models"
-	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	server := gin.Default()
 
 	config.LoadEnv()
 	database.Connect()
 
+	server := gin.Default()
 	server.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:4200"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
@@ -24,27 +22,5 @@ func main() {
 		AllowCredentials: true,
 	}))
 
-	server.GET("/api/notes", func(context *gin.Context) {
-		notes := models.GetAllNotes()
-		context.JSON(http.StatusOK, notes)
-	})
-
-	server.POST("/api/notes", createNote)
-
-	// Start server on port 6969
 	server.Run(":6969")
-}
-
-func createNote(context *gin.Context) {
-	var note models.Note
-	err := context.ShouldBindJSON(&note)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse Data"})
-		return
-	}
-
-	note.ID = 1
-	note.UserID = 1
-	context.JSON(http.StatusCreated, gin.H{"message": "Note Created", "note": note})
 }
