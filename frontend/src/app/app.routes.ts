203a149de2ae17810c80ad1016c975c704acde3d
rotpingo <viorel.moschoi@gmail.com>
2025-08-07 18:07:57 +0200

    added loginPage and RegisterPage components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,8 +3,18 @@ import { Notes } from './components/notes/notes';
 import { Note } from './components/notes/note/note';
 import { Tasks } from './components/tasks/tasks';
 import { Task } from './components/tasks/task/task';
+import { Login } from './components/authentication/login/login';
+import { Register } from './components/authentication/register/register';
 
 export const routes: Routes = [
+  {
+    path: "auth/login",
+    component: Login
+  },
+  {
+    path: "auth/register",
+    component: Register
+  },
   {
     path: "notes",
     component: Notes
