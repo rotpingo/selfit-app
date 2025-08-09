7fcebb5faaa367bc6d1b6211dbc2126dd62b7771
rotpingo <viorel.moschoi@gmail.com>
2025-08-09 13:06:34 +0200

    added TaskResponseDTO

--- backend/dto/task.go
+++ backend/dto/task.go
@@ -1,5 +1,31 @@
 package dto
 
+import (
+	"selfit/models"
+	"time"
+)
+
 type EndTaskDTO struct {
 	Notes string `json:"notes"`
+}
+
+type TaskResponseDTO struct {
+	ID       int64     `json:"id" binding:"required"`
+	Title    string    `json:"title" binding:"required"`
+	Content  string    `json:"content" binding:"required"`
+	IsRepeat bool      `json:"isRepeat" binding:"required"`
+	Interval uint      `json:"interval"`
+	Notes    string    `json:"notes"`
+	DueDate  time.Time `json:"dueDate" binding:"required"`
+}
+
+func TaskToResponseDTO(task *models.Task) TaskResponseDTO {
+	return TaskResponseDTO{
+		ID:       task.ID,
+		Title:    task.Title,
+		Content:  task.Content,
+		IsRepeat: task.IsRepeat,
+		Interval: task.Interval,
+		Notes:    task.Notes,
+	}
 }
